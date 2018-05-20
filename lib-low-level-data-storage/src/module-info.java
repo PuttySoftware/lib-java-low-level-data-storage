3c6ec0c8354596ae0a59589e74bb419680b8f654
Eric Ahnell <eric.ahnell@puttysoftware.com>
2018-05-20 12:54:36 -0400

    Make project a Java module.

---
+++ lib-low-level-data-storage/src/module-info.java
@@ -0,0 +1,11 @@
+/*  LLDS: Arbitrary dimension arrays for Java programs
+Licensed under Apache 2.0. See the LICENSE file for details.
+
+All support is handled via the GitHub repository: https://github.com/wrldwzrd89/lib-java-low-level-data-storage
+ */
+/**
+ *
+ */
+module com.puttysoftware.llds {
+    exports com.puttysoftware.llds;
+}
